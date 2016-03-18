1a51fc9dcad267a6b3f4dd9bca659a7ab1cc46a1
nemo <nemolin@163.com>
2016-03-19 00:40:17 +0800

    fixbug 新建管理员密码没加盐&md5

--- app/controllers/ajax/Admins.java
+++ app/controllers/ajax/Admins.java
@@ -4,12 +4,19 @@ import dao.AdminDao;
 import models.Administrator;
 
 import common.core.AjaxController;
+import utils.EncryptionUtil;
 
 public class Admins extends AjaxController {
+
+    //@TODO 加密盐从配置中获取
+    //加密盐
+    private static final String SLAT = "lllaqenk";
+
     public static void add(Administrator admin) {
         if (admin != null) {
             admin.setCreateTime(System.currentTimeMillis());
             admin.setModifyTime(System.currentTimeMillis());
+            admin.setPassword(EncryptionUtil.md5(admin.getPassword() + SLAT));
             AdminDao.insert(admin);
         }
         renderSuccessJson();
