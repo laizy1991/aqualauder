5e79d046dace4ddeedfc09f5e6d8488fc626cc26
laimt <laimt@jugame.cn>
2016-04-12 23:01:59 +0800

    delete table merchant_trade_order

--- app/controllers/front/Pay.java
+++ app/controllers/front/Pay.java
@@ -22,12 +22,17 @@ import exception.BusinessException;
 
 public class Pay extends FrontController {
 
-	
-    public static void pay(long orderId) throws BusinessException{
+	/**
+	 * 使用微信支付
+	 * @param orderId
+	 * @throws BusinessException
+	 */
+    public static void wxPay(long orderId) throws BusinessException{
     	if(orderId <= 0) {
     		throw new BusinessException(MessageCode.ORDER_ID_INVALID.getMsg());
     	}
     	Order order = OrderService.get(orderId);
+    	//订单检验
     	if(null == order) {
     		throw new BusinessException(MessageCode.ORDER_NULL_ERROR.getMsg());
     	}
@@ -40,6 +45,16 @@ public class Pay extends FrontController {
     	String jsRequestBody = PayService.wxPay(order);
     	BigDecimal b = new BigDecimal(order.getTotalFee()/100D);  
 		double totalFee = b.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
-    	render("/Front/wxPay.html", jsRequestBody, totalFee, order);
+		
+    	render("/Front/Pay/wxPay.html", jsRequestBody, totalFee, order);
+    }
+    
+    /**
+     * 前台回调
+     */
+    public static void frontCallBack() {
+    	//TODO 2016-04-12 23:00 做到这里
+    	//把merchant_trade_order去掉，将字段搬到order表，还没删与merchant_trade_order有关的表与代码
+    	//order的MODEL也还没改，整个创建订单到支付的流程重新梳理一下 
     }
 }
