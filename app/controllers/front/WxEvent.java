21c9e5805cab5f1ffda95ca3593be11cfece235d
laimt <laimt@jugame.cn>
2016-04-28 21:37:54 +0800

    bugfix: 关注事件代码不执行

--- app/controllers/front/WxEvent.java
+++ app/controllers/front/WxEvent.java
@@ -23,18 +23,8 @@ import common.core.FrontController;
 public class WxEvent extends FrontController{
 	public static Gson gson = new Gson();
 	
-	/*public static void valid() {
-		String signature = request.params.get("signature");  
-        String timestamp = request.params.get("timestamp");  
-        String nonce = request.params.get("nonce");  
-        String echostr = request.params.get("echostr");  
-        
-        if(SignUtil.checkSignature(signature, timestamp, nonce)) {
-        	renderJSON(echostr);
-        }
-	}*/
 	/**
-	 * 关注事件
+	 * 微信推送入口
 	 */
 	public static void dispatchEvent() {
 		if(request.method.toUpperCase().equals("GET")) {
@@ -65,7 +55,7 @@ public class WxEvent extends FrontController{
 		}
 		String reqXml = new String(reqStr);
 		if(!StringUtils.isEmpty(reqXml)) {
-			Logger.info("微信事件请求报文为: %s", reqStr);
+			Logger.info("微信事件请求报文为: %s", reqXml);
 			Map<String,Object> xmlMap = null;
 			try {
 				xmlMap = XMLParser.getMapFromXML(reqXml);
@@ -77,17 +67,20 @@ public class WxEvent extends FrontController{
 			//微信服务器在五秒内收不到响应会断掉连接，并且重新发起请求，总共重试三次。
 			if(null != xmlMap) {
 				String event = (String)xmlMap.get("Event");
-				if(StringUtils.isBlank(event)) {
-					Logger.error("获取到的微信事件类型为空");
+				String msgType = (String)xmlMap.get("MsgType");
+				if(StringUtils.isBlank(event) || StringUtils.isBlank(msgType)) {
+					Logger.error("微信推送消息[%s]或事件[%s]类型为空", msgType, event);
 					return;
 				}
-				if(event.toLowerCase().equals("subscribe")) {
+				if(msgType.toLowerCase().equals("event") && event.toLowerCase().equals("subscribe")) {
 					//订阅事件，关注
-					Logger.info("微信事件为订阅事件");
-					//TODO 这下面的方法不执行，也不报错，日了狗了
-					subscribe(reqXml);
+					Logger.info("用户关注事件");
+					if(subscribe(reqXml)) {
+						//入库成功的话，直接回空串，微信不再重试
+						renderJSON("");
+					} 
 				} else {
-					Logger.info("微信事件类型为: %s", event);
+					Logger.info("获取微信推送, 消息[%s], 事件[%s]类型", msgType, event);
 					return;
 				}
 			} else {
@@ -98,36 +91,38 @@ public class WxEvent extends FrontController{
 			Logger.error("微信事件请求报文为空");
 			return;
 		}
-		//服务器无法保证在五秒内处理并回复，可以直接回复空串，微信服务器不会对此作任何处理，并且不会发起重试。
 	}
 	
 	/**
 	 * 订阅事件，关注
 	 * @param reqXml String 请求的XML字符串
 	 */
-	public static void subscribe(String reqXml) {
+	public static boolean subscribe(String reqXml) {
 		if(StringUtils.isBlank(reqXml)) {
 			Logger.info("微信订阅事件传入的参数为空");
-			return;
+			return false;
 		}
 		//XML转为对象
 		SubscribeReqDto req = (SubscribeReqDto)Util.getObjectFromXMLWithXStream(reqXml, SubscribeReqDto.class);
 		if(null == req) {
 			Logger.error("微信订阅事件由XML转换得来的对象为空");
-			return;
+			return false;
 		}
 		String cacheKey = req.getFromUserName()+"_"+req.getCreateTime();	//微信可能会重试多次
 		if(!StringUtils.isBlank((String)Cache.get(cacheKey))) {
 			//前面已成功处理过
 			Logger.info("用户的订阅事件已处理过, 参数: %s", gson.toJson(req));
-			renderJSON("");
+			return true;
 		}
+		Logger.info("开始处理用户关注");
 		boolean subscribedFlag = WxUserService.subscribe(req);
 		if(false == subscribedFlag) {
 			//用户入库失败，不返回信息，让微信重试
-			return;
+			Logger.error("用户关注后信息入库失败，不返回请求给微信");
+			return false;
 		}
+		Logger.info("用户关注后信息入库成功，将openId放入session, openId: %s", req.getFromUserName());
 		session.put("openId", req.getFromUserName());
-		renderJSON("");
+		return true;
 	}
 }
