e55a23231434ba9e060091263f05fb04b76cfa39
daniel <daniel@warthog.cn>
2016-05-18 22:25:24 +0800

    联系客服，模式说明页面可后台编辑

--- app/controllers/admin/HTMLCtrl.java
+++ app/controllers/admin/HTMLCtrl.java
@@ -23,4 +23,32 @@ public class HTMLCtrl extends WebController {
         }
         render("/admin/Html/introduce_edit.html", content);
     }
+    
+    public static void explain() {
+        String content = FileUtils.read("/app/views/Front/help/explain_content.html");
+        render("/admin/Html/explain_edit.html", content);
+    }
+    
+    public static void explainUpdate(String content) {
+        try {
+            FileUtils.write("/app/views/Front/help/explain_content.html", content, false);
+        } catch (IOException e) {
+            Logger.error(e, "");
+        }
+        render("/admin/Html/explain_edit.html", content);
+    }
+    
+    public static void link() {
+        String content = FileUtils.read("/app/views/Front/help/link_content.html");
+        render("/admin/Html/link_edit.html", content);
+    }
+    
+    public static void linkUpdate(String content) {
+        try {
+            FileUtils.write("/app/views/Front/help/link_content.html", content, false);
+        } catch (IOException e) {
+            Logger.error(e, "");
+        }
+        render("/admin/Html/link_edit.html", content);
+    }
 }
