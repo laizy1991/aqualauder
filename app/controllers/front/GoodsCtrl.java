c7e121d6f0e83429d0f8f02c8206704afb9095be
nemo <nemolin@163.com>
2016-05-24 22:29:45 +0800

    商品详情页

--- app/controllers/front/GoodsCtrl.java
+++ app/controllers/front/GoodsCtrl.java
@@ -2,7 +2,7 @@ package controllers.front;
 
 import common.annotation.GuestAuthorization;
 import common.constants.GoodsTag;
-import common.core.FrontController;
+import common.core.WebController;
 import models.Goods;
 import org.apache.commons.lang.StringUtils;
 import play.Logger;
@@ -12,44 +12,23 @@ import service.wx.service.user.WxUserService;
 import java.util.List;
 
 
-public class GoodsCtrl extends FrontController {
+public class GoodsCtrl extends WebController {
 
 	
 	/**
 	 * 微信一级菜单 女神新衣
 	 * 0-新品 1-整体搭配 2-单品 3-鞋帽 4-饰品
-	 * @param tag
-	 * @param code
 	 */
     @GuestAuthorization
-    public static void list(int tag, String code) {
-    	// TODO 从入口处拿取session中的openId，没有的话用code去换，换回来后放入session中
-    	Logger.info("code: %s", code);
-    	String openId = session.get("openId");
-    	if(StringUtils.isBlank(openId)) {
-    		openId = WxUserService.getUserOpenIdByCode(code);
-    	}
-    	switch(tag) {
-			case GoodsTag.NEW:
-				Logger.info("name: 新品, tag: %d", tag);
-				break;
-			case GoodsTag.DRESS:
-				Logger.info("name: 裙装, tag: %d", tag);
-				break;
-			case GoodsTag.SUIT:
-				Logger.info("name: 整体搭配, tag: %d", tag);
-				break;
-			case GoodsTag.TOPS:
-				Logger.info("name: 上装, tag: %d", tag);
-				break;
-			case GoodsTag.BOTTOMS:
-				Logger.info("name: 下装, tag: %d", tag);
-				break;
-			default:
-
-    	}
+    public static void list() {
 
 		List<Goods> goods = GoodsService.list(-1, -1, 0);
 		render("/Front/goods/list.html", goods);
     }
+
+	@GuestAuthorization
+	public static void details(int id) {
+		Goods goods = GoodsService.get(id);
+		render("/Front/goods/details.html", goods);
+	}
 }
