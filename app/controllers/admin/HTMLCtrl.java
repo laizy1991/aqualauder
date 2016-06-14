0375604009a8f54647f22e0bcdd8965efc36b46e
laimt <laimt@jugame.cn>
2016-06-14 11:59:51 +0800

    可编辑页面修改后放入html中

--- app/controllers/admin/HTMLCtrl.java
+++ app/controllers/admin/HTMLCtrl.java
@@ -3,52 +3,110 @@ package controllers.admin;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.lang.StringUtils;
+
 import play.Logger;
 import play.Play;
+import play.mvc.Http.Request;
 import utils.FileUtils;
 import common.core.WebController;
 
 public class HTMLCtrl extends WebController {
-
+	public static String fileDir = Play.applicationPath.getAbsolutePath();
+	public static String introduceFileRelPath = "/public/views/introduce_content.html";
+	public static String explainFileRelPath = "/public/views/explain_content.html";
+	public static String linkFileRelPath = "/public/views/link_content.html";
+	
     public static void introduce() {
-        String content = FileUtils.read("/app/views/Front/help/introduce_content.html");
+        String content = FileUtils.read(introduceFileRelPath);
         render("/admin/Html/introduce_edit.html", content);
     }
     
     public static void introduceUpdate(String content) {
+    	if(StringUtils.isEmpty(content)) {
+    		renderError("内容必须填写");
+    	}
         try {
-            FileUtils.write("/app/views/Front/help/introduce_content.html", content, false);
+        	if(fileDir.endsWith(File.separator)) {
+        		fileDir.substring(0, fileDir.length()-File.separator.length());
+        	}
+        	String fileSrcAbsPath = fileDir + introduceFileRelPath;
+        	String fileBakRelPath = introduceFileRelPath + ".bak";
+        	
+        	File srcFile = new File(fileSrcAbsPath);
+        	String srcContent = "";
+        	if(srcFile.exists()) {
+        		srcContent = FileUtils.read(introduceFileRelPath);
+        		FileUtils.write(fileBakRelPath, srcContent, false);
+        	}
+        	
+            FileUtils.write(introduceFileRelPath, content, false);
         } catch (IOException e) {
             Logger.error(e, "");
         }
+        content = FileUtils.read(introduceFileRelPath);
         render("/admin/Html/introduce_edit.html", content);
     }
     
     public static void explain() {
-        String content = FileUtils.read("/app/views/Front/help/explain_content.html");
+        String content = FileUtils.read(explainFileRelPath);
         render("/admin/Html/explain_edit.html", content);
     }
     
     public static void explainUpdate(String content) {
+        if(StringUtils.isEmpty(content)) {
+    		renderError("内容必须填写");
+    	}
         try {
-            FileUtils.write("/app/views/Front/help/explain_content.html", content, false);
+        	if(fileDir.endsWith(File.separator)) {
+        		fileDir.substring(0, fileDir.length()-File.separator.length());
+        	}
+        	String fileSrcAbsPath = fileDir + explainFileRelPath;
+        	String fileBakRelPath = explainFileRelPath + ".bak";
+        	
+        	File srcFile = new File(fileSrcAbsPath);
+        	String srcContent = "";
+        	if(srcFile.exists()) {
+        		srcContent = FileUtils.read(explainFileRelPath);
+        		FileUtils.write(fileBakRelPath, srcContent, false);
+        	}
+        	
+            FileUtils.write(explainFileRelPath, content, false);
         } catch (IOException e) {
             Logger.error(e, "");
         }
+        content = FileUtils.read(explainFileRelPath);
         render("/admin/Html/explain_edit.html", content);
     }
     
     public static void link() {
-        String content = FileUtils.read("/app/views/Front/help/link_content.html");
+        String content = FileUtils.read(linkFileRelPath);
         render("/admin/Html/link_edit.html", content);
     }
     
     public static void linkUpdate(String content) {
+        if(StringUtils.isEmpty(content)) {
+    		renderError("内容必须填写");
+    	}
         try {
-            FileUtils.write("/app/views/Front/help/link_content.html", content, false);
+        	if(fileDir.endsWith(File.separator)) {
+        		fileDir.substring(0, fileDir.length()-File.separator.length());
+        	}
+        	String fileSrcAbsPath = fileDir + linkFileRelPath;
+        	String fileBakRelPath = linkFileRelPath + ".bak";
+        	
+        	File srcFile = new File(fileSrcAbsPath);
+        	String srcContent = "";
+        	if(srcFile.exists()) {
+        		srcContent = FileUtils.read(linkFileRelPath);
+        		FileUtils.write(fileBakRelPath, srcContent, false);
+        	}
+        	
+            FileUtils.write(linkFileRelPath, content, false);
         } catch (IOException e) {
             Logger.error(e, "");
         }
+        content = FileUtils.read(linkFileRelPath);
         render("/admin/Html/link_edit.html", content);
     }
 }
