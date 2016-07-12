b40841e70f893e5fd048c6a75b196fc19d7edf7d
laimt <laimt@jugame.cn>
2016-07-12 23:06:20 +0800

    微信前端添加更多

---
+++ app/controllers/front/GoodsTypeCtrl.java
@@ -0,0 +1,16 @@
+package controllers.front;
+
+import common.annotation.GuestAuthorization;
+
+public class GoodsTypeCtrl {
+	
+	/**
+	 * 索引列表
+	 * @param tag
+	 * @param fromPage
+	 */
+	@GuestAuthorization
+	public static void list() {
+		
+	}
+}
