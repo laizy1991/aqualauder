6a5ae0887e7e95bd1f026c43393dc0fff9f5b5b5
nemo <nemolin@163.com>
2016-07-19 10:13:57 +0800

    退款处理

--- app/controllers/ajax/RefundOrderCtrl.java
+++ app/controllers/ajax/RefundOrderCtrl.java
@@ -3,7 +3,9 @@ package controllers.ajax;
 import common.core.AjaxController;
 import exception.BusinessException;
 import models.RefundOrder;
+import play.Logger;
 import service.RefundOrderService;
+import service.SellerService;
 
 public class RefundOrderCtrl extends AjaxController {
 
@@ -17,9 +19,18 @@ public class RefundOrderCtrl extends AjaxController {
         renderSuccessJson();
     }
 
-    public static void update(RefundOrder refundOrder) {
-        RefundOrderService.update(refundOrder);
-        renderSuccessJson();
+    public static void update(RefundOrder refundOrder, int isAgree) {
+        if(refundOrder.getId() != null && (isAgree == 1 || isAgree == 2)) {
+            try{
+                if(SellerService.refundAudit(refundOrder.getId(), isAgree, refundOrder.getSellerMemo())) {
+                    renderSuccessJson();
+                    return;
+                }
+            } catch (Exception e) {
+                Logger.error("refund audit fail. id:%s", refundOrder.getOrderId(), e.getMessage());
+            }
+            renderErrorJson("处理退款单失败");
+        }
     }
 
     
