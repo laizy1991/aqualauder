7db9689e9d4c7b7bf4968f0e72cdaa5fbc9013a3
laimt <laimt@jugame.cn>
2016-09-10 17:29:29 +0800

    添加事务支持

--- app/controllers/ajax/RefundOrderCtrl.java
+++ app/controllers/ajax/RefundOrderCtrl.java
@@ -1,11 +1,10 @@
 package controllers.ajax;
 
+import play.db.jpa.JPA;
+import models.RefundOrder;
+import service.SellerService;
 import common.core.AjaxController;
 import exception.BusinessException;
-import models.RefundOrder;
-import play.Logger;
-import service.RefundOrderService;
-import service.SellerService;
 
 public class RefundOrderCtrl extends AjaxController {
 
@@ -20,17 +19,21 @@ public class RefundOrderCtrl extends AjaxController {
     }
 
     public static void update(RefundOrder refundOrder, int isAgree) {
-        if(refundOrder.getId() != null && (isAgree == 1 || isAgree == 2)) {
-            try{
-                if(SellerService.refundAudit(refundOrder.getId(), isAgree, refundOrder.getSellerMemo())) {
-                    renderSuccessJson();
-                    return;
-                }
-            } catch (Exception e) {
-                Logger.error("refund audit fail. id:%s", refundOrder.getOrderId(), e.getMessage());
-            }
-            renderErrorJson("处理退款单失败");
-        }
+    	try {
+    		if(refundOrder.getId() != null && (isAgree == 1 || isAgree == 2)) {
+    			if(SellerService.refundAudit(refundOrder.getId(), isAgree, refundOrder.getSellerMemo())) {
+    				renderSuccessJson();
+    				return;
+    			}
+    		}
+    	} catch(Exception e) {
+    		JPA.setRollbackOnly();
+    		e.printStackTrace();
+    		if(e instanceof BusinessException) {
+    			renderErrorJson(e.getMessage());
+    		}
+    	}
+        renderErrorJson("退款失败，服务器异常");
     }
 
     
--- app/service/SellerService.java
+++ app/service/SellerService.java
@@ -1,23 +1,22 @@
 package service;
 
-import javax.persistence.Query;
-
-import com.google.gson.Gson;
-
-import common.constants.OrderStatus;
-import common.constants.PayType;
-import common.constants.RefundStatus;
-import common.constants.wx.WxRefundStatus;
-import dao.ExpressDao;
-import exception.BusinessException;
 import models.Express;
 import models.Order;
 import models.RefundOrder;
 import play.Logger;
-import play.db.jpa.Model;
 import service.wx.WXPay;
 import service.wx.dto.refund.SendRefundReqDto;
 import service.wx.dto.refund.SendRefundRspDto;
+
+import com.google.gson.Gson;
+import common.constants.OrderStatus;
+import common.constants.PayType;
+import common.constants.RefundStatus;
+import common.constants.wx.WxRefundStatus;
+
+import dao.ExpressDao;
+import dto.WxMsgRspDto;
+import exception.BusinessException;
 
 public class SellerService {
 	private static Gson gson = new Gson();
@@ -32,7 +31,8 @@ public class SellerService {
     public static boolean refundAudit(long refundId, int isAgree, String reason) throws BusinessException {
         RefundOrder refundOrder = RefundOrderService.get(refundId);
         if(refundOrder == null) {
-            return false;
+        	throw new BusinessException("退款失败，无法获取退款单, refundId: "+refundId);
+//            return false;
         }
         
         RefundStatus refundState = RefundStatus.ING;
@@ -43,7 +43,8 @@ public class SellerService {
             boolean isSucc = OrderService.setStatusAndUpdate(refundOrder.getOrderId(), OrderStatus.CLOSE);
             if(!isSucc) {
                 Logger.error("close order fail. id:%s", refundOrder.getOrderId());
-                return false;
+                throw new BusinessException("退款失败，准备关闭订单时出错, refundId: "+refundId);
+//                return false;
             }
         }
 
@@ -63,17 +64,22 @@ public class SellerService {
 				ro.getRefundState() == RefundStatus.NOTREFUND.getCode()) {
     		Logger.info("该笔订单状态为%s, 禁止退款，refundId: %d", RefundStatus.resolveType(ro.getRefundState()).getDesc(),
     				ro.getId());
+    		throw new BusinessException("退款失败, refundId: " + ro.getId() + ", 原因：退款单不符合退款状态");
     	}
     	Order order = OrderService.get(ro.getOrderId());
     	if(null == order) {
     		Logger.error("查询订单记录失败，orderId: %d", ro.getOrderId());
+    		throw new BusinessException("退款失败, refundId: " + ro.getId() + ", 原因：查询订单记录失败，orderId: " + ro.getOrderId());
+    	}
+    	if(order.getForbidRefund() == 1) {
+    		throw new BusinessException("退款失败, refundId: " + ro.getId() + ", 原因：该订单禁止退款，orderId: " + ro.getOrderId());
     	}
     	SendRefundReqDto req = new SendRefundReqDto(order.getPlatformTransationId(), ro.getOutRefundNo(), 
     				order.getTotalFee(), order.getTotalFee(), String.valueOf(order.getUserId()));
     	SendRefundRspDto rsp = WXPay.sendRefundServcie(req);
     	Logger.info("rsp:" + new Gson().toJson(rsp));
     	if(null != rsp && rsp.getReturn_code().equals("SUCCESS") && !rsp.getResult_code().equals(WxRefundStatus.FAIL.getType())) {
-    		Logger.info("申请退款成功，退款单参数为：%s", gson.toJson(ro));
+    		Logger.info("退款成功, refundId: " + ro.getId() + ", 退款单参数为：%s", gson.toJson(ro));
     		//更新退款单
     		ro.setRefundId(rsp.getRefund_id());
     		ro.setRefundState(RefundStatus.SUCCESS.getCode());
@@ -81,10 +87,11 @@ public class SellerService {
     		ro.setRefundRecvAccout(PayType.WX.getDesc());
     		RefundOrderService.update(ro);
         	//发送通知
-        	WxMsgService.refundMoneyResultMsg(ro.getId());
-    	} else {
-    		Logger.error("退款失败，退款单参数为：%s", gson.toJson(ro));
+    		WxMsgService.refundMoneyResultMsg(ro.getId());
+    		return;  
     	}
+    	Logger.error("退款失败，退款单参数为：%s", gson.toJson(ro));
+    	throw new BusinessException("退款失败, refundId: " + ro.getId() + ", 原因为: " + (null==rsp?"微信返回退款信息为空":rsp.getReturn_msg()));
     }
     
     /**
